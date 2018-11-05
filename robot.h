071fc46718b06822afcfe8ca8fe300e26aa33458
Edwin Miguel Lara Espinoza <em.laraespinoza@hotmail.com>
2018-11-04 22:03:22 -0600

    corrigiendo cabecera de robot

--- robot.h
+++ robot.h
@@ -1,40 +1,70 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
+#ifndef ROBOT_H
+#define ROBOT_H
+ 
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <vector>
 #include <cmath>
 #include <iostream>
-
+#include <cstdlib>
+ 
 using namespace std;
-
+ 
 class Robot
 {
-public:
+private:
     float angulo;
     float v1;
     float v2;
     float diametro;
     float radio;
+ 
+    float *posINI;
+    float *posNEW;
+ 
+    float **matTrans;
+ 
+ 
+public:
+    int bandera;
+ 
     vector <float> posglobal;
     vector <float> posRefencial;
-
+ 
     GLfloat rtraslateX;
     GLfloat rtraslateY;
-
-
+    GLfloat rotate;
+ 
+ 
     Robot();
     void pintar_robot();
     int obtener_instruccion(int key, int x, int y);
-
-    void mover_derecha();
+ 
+    float* mover_derecha();
     void mover_izquierda();
     void mover_arriba();
     void mover_abajo();
-
+ 
     void TraslateX(float value);
     void TraslateY(float value);
+    void rotateRobot(float value);
+ 
+    void setV1(float v1);
+    float getV1(void);
+ 
+    void setV2(float v2);
+    float getV2(void);
+ 
+    void setAngulo(float an);
+    float getAngulo(void);
+ 
+    void crearmatriz(void);
+    void crearMaTRAnd(float angulo);
+ 
+ 
 };
-
+ 
 #endif // ROBOT_H
